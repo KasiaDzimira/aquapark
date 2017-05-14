5a2b7c16b3f4c958e294c9794999027f41b2655d
Grzegorz Stoklosa <stoklosa.grzegorz@itsilesia.com>
2017-05-14 22:13:55 +0200

    Add attraction type controller

--- src/Controller/AttractionTypeController.java
+++ src/Controller/AttractionTypeController.java
@@ -1,7 +1,31 @@
 package Controller;
 
-/**
- * Created by Grzesiek on 14.05.2017.
- */
+import Database.Connector;
+
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class AttractionTypeController {
+    private Connector connector;
+
+    public AttractionTypeController() {
+        this.connector = new Connector();
+    }
+
+    public void createAttractionType(String name, BigDecimal price) {
+        this.connector.connect();
+        try {
+            Statement st = this.connector.getConnection().createStatement();
+            String sql = "INSERT INTO attraction_type (name, price) VALUES ('" +
+                    name + "'," +
+                    price + ")";
+            st.executeUpdate(sql);
+            this.connector.getConnection().commit();
+            System.out.println("Query has been executed");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        this.connector.closeConnection(null);
+    }
 }
