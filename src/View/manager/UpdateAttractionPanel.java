fbfe7d970167c16a28d7899f4952065b189049fd
homik3pl <homik3pl@gmail.com>
2017-06-26 00:37:06 +0200

    'create pass and details view' added

--- src/View/manager/UpdateAttractionPanel.java
+++ src/View/manager/UpdateAttractionPanel.java
@@ -4,7 +4,6 @@ import Controller.AttractionController;
 import Controller.AttractionTypeController;
 import Model.Attraction;
 import Model.AttractionType;
-import jdk.nashorn.internal.scripts.JO;
 
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
@@ -14,10 +13,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 
 public class UpdateAttractionPanel extends JPanel {
     private JList<Attraction> attractionList;
